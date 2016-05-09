e6267d67f91b58bdf4e20298f24c2464ff90c5c1
lwin khine mon <lkm.tokyo@gmail.com>
2016-05-09 18:09:33 +0900

    today

---
+++ 20160509/012_define2.php
@@ -0,0 +1,8 @@
+<?php
+
+define("kazu", 87);
+echo kazu ."\n";
+
+define("kazu", 99);
+echo kazu ."\n";
+?>
---
+++ 20160509/017_dot2.php
@@ -0,0 +1,9 @@
+<?php
+// 変数に文字列を格納
+$str_name1 = "lwin";
+$str_name2 = "khinemon";
+
+// 文字列を連結して出力
+echo $str_name1 . "はみょうじだ！。\n";
+echo $str_name2 . "は名前だ！。\n";
+?>
